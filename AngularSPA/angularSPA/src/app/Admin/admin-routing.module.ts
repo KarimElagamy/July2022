810bd8710cc0fc0fc165b9345f6ccd7865680cea
KarimElagamy <102981217+KarimElagamy@users.noreply.github.com>
2022-09-13 17:13:10 -0400

    Angular day 3 push

--- AngularSPA/angularSPA/src/app/Admin/admin-routing.module.ts
+++ AngularSPA/angularSPA/src/app/Admin/admin-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddGenreComponent } from './add-genre.component';
+import { CreateCastComponent } from './create-cast.component';
+import { CreateMovieComponent } from './create-movie.component';
+import { TopPurchasesComponent } from './top-purchases.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "Add-Genre", component: AddGenreComponent },
+  { path: "Create-Cast", component: CreateCastComponent },
+  { path: "Create-Movie", component: CreateMovieComponent },
+  { path: "Top-Purchases", component: TopPurchasesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
